a47788aa3e27a94a919972feaa14a83e6b101ef5
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-01-31 05:39:05 -0500

    Initial commit

--- be-transrendered.js
+++ be-transrendered.js
@@ -1,10 +1,15 @@
+import { define } from 'be-decorated/DE.js';
+import { register } from "be-hive/register.js";
 export class BeTransrendered extends EventTarget {
     async instantiate(pp, mold) {
         //TODO lots of common code with be-free-ranged.  maybe move some to be-transformed package.
         const { host, transformIslets, self, template } = pp;
         const { DTR } = await import('trans-render/lib/DTR.js');
         const { getDestructArgs } = await import('trans-render/lib/getDestructArgs.js');
-        const clone = template.content.cloneNode(true);
+        let clone;
+        if (self.innerHTML.trim() === '') {
+            clone = template.content.cloneNode(true);
+        }
         const { IsletTransformer } = await import('./IsletTransformer.js');
         for (const transformIslet of transformIslets) {
             const { transform, islet } = transformIslet;
@@ -25,13 +30,41 @@ export class BeTransrendered extends EventTarget {
             if (islet !== undefined) {
                 Object.assign(host, islet(host));
             }
-            await transformer.transform(clone);
-            self.innerHTML = '';
-            self.appendChild(clone);
+            if (clone !== undefined) {
+                await transformer.transform(clone);
+            }
             for (const transformIslet of transformIslets) {
                 new IsletTransformer(self, transformIslet, host);
             }
         }
+        if (clone !== undefined) {
+            self.appendChild(clone);
+        }
         return mold;
     }
 }
+const tagName = 'be-transrendered';
+const ifWantsToBe = 'transrendered';
+const upgrade = '*';
+define({
+    config: {
+        tagName,
+        propDefaults: {
+            ifWantsToBe,
+            upgrade,
+            virtualProps: ['transformIslets', 'host', 'template']
+        },
+        actions: {
+            instantiate: {
+                ifAllOf: ['transformIslets', 'host', 'template'],
+                returnObjMold: {
+                    resolved: true,
+                }
+            }
+        }
+    },
+    complexPropDefaults: {
+        controller: BeTransrendered
+    }
+});
+register(ifWantsToBe, upgrade, tagName);
--- be-transrendered.ts
+++ be-transrendered.ts
@@ -1,6 +1,7 @@
 import {define, BeDecoratedProps} from 'be-decorated/DE.js';
 import {Proxy, PP, Actions, VirtualProps, PPP, PPE, PPPP} from './types';
 import {RenderContext} from 'trans-render/lib/types';
+import {register} from "be-hive/register.js";
 
 export class BeTransrendered extends EventTarget implements Actions{
     async instantiate(pp: PP, mold: Partial<PP>): PPPP {
@@ -8,7 +9,10 @@ export class BeTransrendered extends EventTarget implements Actions{
         const {host, transformIslets, self, template} = pp;
         const {DTR} = await import('trans-render/lib/DTR.js');
         const {getDestructArgs} = await import('trans-render/lib/getDestructArgs.js');
-        const clone = template!.content.cloneNode(true) as DocumentFragment;
+        let clone: DocumentFragment | undefined;
+        if(self.innerHTML.trim() === ''){
+            clone = template!.content.cloneNode(true) as DocumentFragment;
+        }
         const {IsletTransformer} = await import('./IsletTransformer.js');
         for(const transformIslet of transformIslets!){
             const {transform, islet} = transformIslet;
@@ -31,13 +35,45 @@ export class BeTransrendered extends EventTarget implements Actions{
             if(islet !== undefined){
                 Object.assign(host!, islet(host));
             }
-            await transformer.transform(clone);
-            self.innerHTML = '';
-            self.appendChild(clone);
+            if(clone !== undefined){
+                await transformer.transform(clone);
+                
+            }
+
             for(const transformIslet of transformIslets!){
                 new IsletTransformer(self, transformIslet, host!);
             }
         }
+        if(clone !== undefined){
+            self.appendChild(clone);
+        }
         return mold;
     }
 }
+
+const tagName = 'be-transrendered';
+const ifWantsToBe = 'transrendered';
+const upgrade = '*';
+
+define<Proxy & BeDecoratedProps<Proxy, Actions>, Actions>({
+    config: {
+        tagName,
+        propDefaults: {
+            ifWantsToBe,
+            upgrade,
+            virtualProps: ['transformIslets', 'host', 'template']
+        },
+        actions:{
+            instantiate: {
+                ifAllOf: ['transformIslets', 'host', 'template'],
+                returnObjMold: {
+                    resolved: true,
+                }
+            }
+        }
+    },
+    complexPropDefaults: {
+        controller: BeTransrendered
+    }
+});
+register(ifWantsToBe, upgrade, tagName);
